3e0c83ef46486deb11ba4deba8c110ab01af1d23
mohit131312 <Mohit@appcartsystems.com>
2025-08-01 16:12:21 +0530

    last push

--- lib/remote_service.dart
+++ lib/remote_service.dart
@@ -17,10 +17,10 @@ class RemoteServices {
     await gs.erase();
   }
 
-  // static const String baseUrl =
-  //     "http://192.168.1.72/Kumar/KumarProperties/api/v1/"; // WIFI
   static const String baseUrl =
-      "http://34.133.129.206/stagging/api/v1/"; //stagging
+      "http://192.168.1.72/Kumar/KumarProperties/api/v1/"; // WIFI
+  // static const String baseUrl =
+  //     "http://34.133.129.206/stagging/api/v1/"; //stagging
   // static const String baseUrl =
   //     "http://34.133.129.206/stagging/api/v1/"; //stagging
 
